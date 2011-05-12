4569efe0b06b7a2f00f9c7a7f5b3a8198701cba6
Michael Nutt <michael@nuttnet.net>
2011-05-12 15:36:11 -0400

    add query argument logging

--- lib/mongo/rails/instrumentation/log_subscriber.rb
+++ lib/mongo/rails/instrumentation/log_subscriber.rb
@@ -14,7 +14,39 @@ class Mongo::Rails::Instrumentation::LogSubscriber < ActiveSupport::LogSubscribe
     rt
   end
 
+  def initialize
+    super
+
+    @odd_or_even = false
+  end
+
   def mongo(event)
     self.class.runtime += event.duration
+  end
+
+  def query(event)
+    return unless logger.debug?
+
+    collection, options = event.payload[:cursor]
+
+    name = "MONGO QUERY"
+
+    query = format_query options.delete(:selector)
+    extra = format_query options
+
+    debug "  #{color(name, YELLOW, true)} #{collection.name} { #{query} } [ #{extra} ]"
+  end
+
+  # produces: 'query: "foo" OR "bar", rows: 3, ...'
+  def format_query(query)
+    query.map{ |k, v| "#{k}: #{color(v, BOLD, true)}" if v.present? }.compact.join(', ')
+  end
+
+  def odd?
+    @odd_or_even = !@odd_or_even
+  end
+
+  def logger
+    Rails.logger
   end
 end
--- lib/mongo/rails/instrumentation/railtie.rb
+++ lib/mongo/rails/instrumentation/railtie.rb
@@ -4,10 +4,14 @@ module Mongo::Rails::Instrumentation
   class Railtie < Rails::Railtie
     initializer "mongo.rails.instrumentation" do |app|
       instrument Mongo::Connection, [
-        :send_message,
-        :send_message_with_safe_check,
-        :receive_message
-      ]
+                                     :send_message,
+                                     :send_message_with_safe_check,
+                                     :receive_message
+                                    ], "mongo"
+
+      instrument Mongo::Cursor, [
+                                 :initialize
+                                ], "query"
 
       ActiveSupport.on_load(:action_controller) do
         include ControllerRuntime
@@ -16,11 +20,11 @@ module Mongo::Rails::Instrumentation
       LogSubscriber.attach_to :mongo
     end
 
-    def instrument(clazz, methods)
+    def instrument(clazz, methods, name)
       clazz.module_eval do
         methods.each do |m|
           class_eval %{def #{m}_with_instrumentation(*args, &block)
-            ActiveSupport::Notifications.instrumenter.instrument "mongo.mongo", :name => "#{m}" do
+            ActiveSupport::Notifications.instrumenter.instrument "#{name}.mongo", :name => "#{m}", :cursor => args do
               #{m}_without_instrumentation(*args, &block)
             end
           end
